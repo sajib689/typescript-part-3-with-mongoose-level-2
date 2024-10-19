d9525ce1be710049ab1383f1e44b5e2cde0bdfa2
sajib689 <jibonraja11@gmail.com>
2024-10-19 08:36:36 +0600

    add intterface

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,37 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { Schema } from 'mongoose';
 const app:Application = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended: true}))
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello world");
+  // create a new interface
+  interface IUser {
+    id: string;
+    role: 'student';
+    password: string;
+    name: {
+      firstName: string,
+      middleName: string,
+      lastName: string,
+    }
+    dateOfBirth?: string;
+    gender: 'male' | 'female';
+    email?: string;
+    contactNumber?: string;
+    emergencyContactNumber?: string;
+    presentAddress?: string;
+    permanentAddress?: string;
+  }
+  // creating a new Schema object
+
+  const useSchema = new Schema <IUser> ({
+    
+  })
+
+
+    
   });
 
   export default app;
